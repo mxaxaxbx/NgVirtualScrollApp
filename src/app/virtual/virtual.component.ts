1dbc7c2381095218ada00eee3f4865f14381b2fa
Miguel Arenas <marenas@servinf.local>
2020-12-21 12:55:05 -0500

    first commit

---
+++ src/app/virtual/virtual.component.ts
@@ -0,0 +1,34 @@
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styleUrls: ['./virtual.component.css']
+})
+export class VirtualComponent implements OnInit {
+
+  @ViewChild( CdkVirtualScrollViewport ) viewport: CdkVirtualScrollViewport;
+
+  personas = Array(500).fill(0);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.personas);
+    
+  }
+
+  irAlFinal() {
+    this.viewport.scrollToIndex( this.personas.length );
+  }
+
+  irAlInicio() {
+    this.viewport.scrollToIndex( this.personas[0] );
+  }
+
+  irALaMitad() {
+    this.viewport.scrollToIndex( this.personas.length / 2 );
+  }
+
+}
